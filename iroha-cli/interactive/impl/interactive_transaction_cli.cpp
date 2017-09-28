4dcea7c1f94089ccbc3a097c8da06795d8223484
luckychess <toobwn@gmail.com>
2017-09-28 16:03:24 +0300

    Signing transaction on a client side and verifying them in stateless validator.

--- iroha-cli/interactive/impl/interactive_transaction_cli.cpp
+++ iroha-cli/interactive/impl/interactive_transaction_cli.cpp
@@ -19,10 +19,13 @@
 #include <fstream>
 #include "model/converters/json_common.hpp"
 #include "model/converters/json_transaction_factory.hpp"
+#include "model/converters/pb_transaction_factory.hpp"
 #include "model/generators/transaction_generator.hpp"
 
 #include <chrono>
 #include "client.hpp"
+#include "crypto/crypto.hpp"
+#include "crypto/hash.hpp"
 #include "grpc_response_handler.hpp"
 #include "model/commands/append_role.hpp"
 #include "model/commands/create_role.hpp"
@@ -52,8 +55,7 @@ namespace iroha_cli {
           {CREATE_ROLE, "Create new role"},
           {APPEND_ROLE, "Add new role to account"},
           {GRANT_PERM, "Grant permission over your account"},
-          {REVOKE_PERM, "Revoke permission from account"}
-      };
+          {REVOKE_PERM, "Revoke permission from account"}};
 
       const auto acc_id = "Account Id";
       const auto ast_id = "Asset Id";
@@ -86,8 +88,7 @@ namespace iroha_cli {
           {CREATE_ROLE, {role}},
           {APPEND_ROLE, {acc_id, role}},
           {GRANT_PERM, {acc_id, perm}},
-          {REVOKE_PERM, {acc_id, perm}}
-      };
+          {REVOKE_PERM, {acc_id, perm}}};
 
       command_handlers_ = {
           {ADD_ASSET_QTY, &InteractiveTransactionCli::parseAddAssetQuantity},
@@ -105,8 +106,7 @@ namespace iroha_cli {
           {CREATE_ROLE, &InteractiveTransactionCli::parseCreateRole},
           {APPEND_ROLE, &InteractiveTransactionCli::parseAppendRole},
           {GRANT_PERM, &InteractiveTransactionCli::parseGrantPermission},
-          {REVOKE_PERM, &InteractiveTransactionCli::parseGrantPermission}
-      };
+          {REVOKE_PERM, &InteractiveTransactionCli::parseGrantPermission}};
 
       commands_menu_ = formMenu(command_handlers_, command_params_descriptions_,
                                 commands_description_map_);
@@ -134,17 +134,18 @@ namespace iroha_cli {
           {SAVE_CODE, &InteractiveTransactionCli::parseSaveFile},
           {SEND_CODE, &InteractiveTransactionCli::parseSendToIroha},
           {ADD_CMD, &InteractiveTransactionCli::parseAddCommand},
-          {BACK_CODE, &InteractiveTransactionCli::parseGoBack}
-      };
+          {BACK_CODE, &InteractiveTransactionCli::parseGoBack}};
 
       result_menu_ = formMenu(result_handlers_, result_params_descriptions,
                               result_desciption);
     }
 
     InteractiveTransactionCli::InteractiveTransactionCli(
-        std::string creator_account, uint64_t tx_counter) {
-      creator_ = creator_account;
-      tx_counter_ = tx_counter;
+        std::string creator_account, uint64_t tx_counter)
+        : creator_(creator_account),
+          tx_counter_(tx_counter),
+          keysManager_(creator_account) {
+      log_ = logger::log("InteractiveTransactionCli");
       createCommandMenu();
       createResultMenu();
     }
@@ -195,7 +196,7 @@ namespace iroha_cli {
         std::vector<std::string> params) {
       // TODO: implement scheme on working with permissions
       auto role = params[0];
-      std::vector<std::string> perms  = {};
+      std::vector<std::string> perms = {};
       return std::make_shared<CreateRole>(role, perms);
     }
 
@@ -249,8 +250,7 @@ namespace iroha_cli {
       auto address = params[0];
       auto key = params[1];
       iroha::pubkey_t pubkey;
-      pubkey = iroha::hexstringToArray<iroha::pubkey_t::size()>(key)
-                   .value();
+      pubkey = iroha::hexstringToArray<iroha::pubkey_t::size()>(key).value();
       return generator_.generateAddPeer(address, pubkey);
     }
 
@@ -260,8 +260,7 @@ namespace iroha_cli {
       auto account_id = params[0];
       auto key = params[1];
       iroha::pubkey_t pubkey;
-      pubkey = iroha::hexstringToArray<iroha::pubkey_t::size()>(key)
-                   .value();
+      pubkey = iroha::hexstringToArray<iroha::pubkey_t::size()>(key).value();
       return generator_.generateAddSignatory(account_id, pubkey);
     }
 
@@ -272,8 +271,7 @@ namespace iroha_cli {
       auto domain_id = params[1];
       auto key = params[2];
       iroha::pubkey_t pubkey;
-      pubkey = iroha::hexstringToArray<iroha::pubkey_t::size()>(key)
-                   .value();
+      pubkey = iroha::hexstringToArray<iroha::pubkey_t::size()>(key).value();
       return generator_.generateCreateAccount(account_id, domain_id, pubkey);
     }
 
@@ -303,8 +301,7 @@ namespace iroha_cli {
       auto account_id = params[0];
       auto key = params[1];
       iroha::pubkey_t pubkey;
-      pubkey = iroha::hexstringToArray<iroha::pubkey_t::size()>(key)
-                   .value();
+      pubkey = iroha::hexstringToArray<iroha::pubkey_t::size()>(key).value();
       return generator_.generateRemoveSignatory(account_id, pubkey);
     }
 
@@ -375,7 +372,20 @@ namespace iroha_cli {
           std::chrono::system_clock::now().time_since_epoch() / 1ms;
       auto tx = tx_generator_.generateTransaction(time_stamp, creator_,
                                                   tx_counter_, commands_);
-      // TODO: sign tx
+      auto keys = keysManager_.loadKeys();
+      if (keys) {
+        auto sig = iroha::sign(iroha::hash(tx).to_string(), keys->pubkey,
+                               keys->privkey);
+        tx.signatures.push_back(
+            Signature{.signature = sig, .pubkey = keys->pubkey});
+      } else {
+        // TODO: check what should we do - generate new keys or return an error
+        // or may be something else
+        log_->warn(
+            "Could not load keypair for {}, transaction remains unsigned",
+            creator_);
+      }
+
       CliClient client(address.value().first, address.value().second);
       GrpcResponseHandler response_handler;
       response_handler.handle(client.sendTx(tx));
@@ -398,7 +408,19 @@ namespace iroha_cli {
           std::chrono::system_clock::now().time_since_epoch() / 1ms;
       auto tx = tx_generator_.generateTransaction(time_stamp, creator_,
                                                   tx_counter_, commands_);
-      // TODO: sign tx
+      auto keys = keysManager_.loadKeys();
+      if (keys) {
+        auto sig = iroha::sign(iroha::hash(tx).to_string(), keys->pubkey,
+                               keys->privkey);
+        tx.signatures.push_back(
+            Signature{.signature = sig, .pubkey = keys->pubkey});
+      } else {
+        // TODO: check what should we do - generate new keys or return an error
+        // or may be something else
+        log_->warn(
+            "Could not load keypair for {}, transaction remains unsigned",
+            creator_);
+      }
 
       iroha::model::converters::JsonTransactionFactory json_factory;
       auto json_doc = json_factory.serialize(tx);
